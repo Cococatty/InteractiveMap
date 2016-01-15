1fa2b53fd1de99edddfef5384a0ceaacd7684ad8
Cococatty <yzh215@mathxeon7.math.canterbury.ac.nz>
2016-01-15 14:19:13 +1300

    WORKING VERSION (finally found it...)

--- Shiny/combined.R
+++ Shiny/combined.R
@@ -1,3 +1,9 @@
+# 
+# pck <- c("shiny", "DT")
+# require(pck) || install.packages(pck)
+# 
+# library(shiny)
+# library(DT)
 
 require("shiny") || install.packages("shiny")
 require("DT") || install.packages("DT")
@@ -12,122 +18,148 @@ minGrp <- 1
 maxGrp <- 2
 
 
+#Definte UI for the application
+ui <- fluidPage(
+  sidebarPanel(
+    sliderInput("categories", "Number of Categories", min = 1, max = 10, value = 5
+    )
+    
+    , selectInput(
+      "classIntMethod"#, "Division Method"
+      , label = "Select the intervals"
+      , choices = list("Equal" = "equal"
+                       , "Fisher" = "fisher"
+                       , "Fixed"= "fixed"
+                       , "Pretty" = "pretty"
+                       , "Quantiles" = "quantile"
+                       , "Standard Deviation" = "sd"
+      )
+      , selected = "quantile"
+      #                        , selectize = FALSE
+      , multiple = FALSE
+      #                        , width = "auto"
+    )
+    
+    , br()
+    
+    # The following part is SelectInput format for the TravelMean
+    #         selectInput("travelMean", "Travel Mean"
+    #                       , label = "Select the mean below:"
+    #                       , choices = meanChoices
+    #                       , multiple = TRUE
+    #                       , width = "100%"
+    #         )
+    
+    # The following part is groupCheckBox format for the TravelMean
+    , checkboxGroupInput(
+      "travelMean"
+      #                    , "Travel Mean"
+      , label = "Select the mean below:"
+      , choices = meanChoices
+      , selected = NULL
+      #, inline = FALSE
+    )
+    
+    , br()
+    , height = "10%"
+  ),
+  
+  #Show the map
+  mainPanel(
+    
+    h3("Map of New Zealand", align = "center")
+    , h4(textOutput("text1"), align = "center")
+    , h4(textOutput("text2"), align = "center")
+    , h4("Data is in percentages", align = "center")
+    
+    , tabsetPanel(#type = "tabs",
+      tabPanel("Single-Mean Table", tableOutput("onetable")  )
+      , tabPanel("Single-Mean Plot", plotOutput("oneMap"))
+      , tabPanel("Two-Mean Table", tableOutput("biTable")  )
+      , tabPanel("Two-Mean Plot", plotOutput("biMap"))
+    )
+    
+    , position="center"
+    , width= "auto"
+    , height= "auto"
+  )
+)
+
+
+
 
 #Definte server logic required to draw the map
 server <- function(input, output, session) {
-    output$categories <- renderUI({
-      sliderInput("categories", "Number of Categories", min = 1, max = 10, value = 5)
-    })
-      
-    output$classIntMethod <- renderUI({
-      selectInput("classIntMethod"#, "Division Method"
-        , label = "Select the intervals"
-        , choices = list("Equal" = "equal"
-                         , "Fisher" = "fisher"
-                         , "Fixed"= "fixed"
-                         , "Pretty" = "pretty"
-                         , "Quantiles" = "quantile"
-                         , "Standard Deviation" = "sd"
-        )
-        , selected = "quantile"
-        , multiple = FALSE
-      )
-      
-    })
-      
+  
+  updateoneTable <- reactive({
+    onetable <- subset(newtable, newtable$MeanCode == tail(input$travelMean, 1), select = -c(MeanCode)
+                       , colnames = c('Territory', 'Mean Name', 'Number of People', 'Overall weight'))
+    onetable <- onetable[order(onetable$Percentage),]
+  })
+  
+  output$onetable <- renderTable({
+    updateoneTable()}
+    , include.rownames = FALSE
+    , options = list(paging = FALSE, searching = FALSE)
+    , caption = paste("Travel mean: ", tail(input$travelMean, 1))
+    , caption.placement = getOption("xtable.caption.placement", "top")
+    , caption.width = getOption("xtable.caption.width", NULL)
+    #, colnames = c('Territory', 'Mean Name', 'Number of People', 'Overall weight')
+  )
+  #test <- reactive({ length(input$travelMean)})
+  #test1 <- reactive({ input$travelMean})
+  #test <- reactive({summary(input$travelMean) })
+  #test
+  observe({
+    if (length(input$travelMean) > maxGrp)
+    {
+      updateCheckboxGroupInput(session, 'travelMean', selected = tail(input$travelMean,maxGrp))
+    }
+  })
+  
+  #    updatebiTable <- reactive({
+  
+  #       biTable <- subset(newtable, newtable$MeanCode == input$travelMean, select = -c(MeanCode)
+  #                          , colnames = c('Territory', 'Mean Name', 'Number of People', 'Overall weight'))
+  #       biTable <- biTable[order(biTable$Percentage),]
+  #    })
+  #     
+  test <- xtabs(as.numeric(Ppl) ~ AreaCode + MeanCode, data=geodata)
+  #test <- test[!(test$dimnames$AreaCode == 'MeanCode' & test$dimnames$MeanCode == 'AreaCode'), ]
+  #test <- test[!(test$MeanCode == 'MeanCode' & test$AreaCode == 'AreaCode'), ]
+  #test <- as.table(test, dnn=c("MeanCode", "AreaCode"))
+  
+  #remove(test)
+  #head(test)
+  #      head(warpbreaks)
+  #      warpbreaks$replicate <- rep(1:9, len = 54)
+  #      ftable(xtabs(breaks ~ wool + tension + replicate, data = warpbreaks))
+  
+  
+  
+  output$biTable <- renderTable({
+    test
     
-    output$travelMean <- renderUI({
-      # The following part is groupCheckBox format for the TravelMean
-      checkboxGroupInput("travelMean"
-        , "Travel Mean"
-        , label = "Select the mean below:"
-        , choices = meanChoices
-        , selected = NULL
-      )
-    })
-    
-      
-    updateoneTable <- reactive({
-      onetable <- subset(newtable, newtable$MeanCode == tail(input$travelMean, 1), select = -c(MeanCode)
-                         , colnames = c('Territory', 'Mean Name', 'Number of People', 'Overall weight'))
-      onetable <- onetable[order(onetable$Percentage),]
-    })
-    
-    output$onetable <- renderTable({
-      updateoneTable()}
-      , include.rownames = FALSE
-      , options = list(paging = FALSE, searching = FALSE)
-      , caption = paste("Travel mean: ", tail(input$travelMean, 1))
-      , caption.placement = getOption("xtable.caption.placement", "top")
-      , caption.width = getOption("xtable.caption.width", NULL)
-    )
-    
-
-    
-    output$biTable <- renderTable({
-      biTableDraft <- biTableDraft[(geodata$MeanCode %in% input$travelMean), ]
-      #biTableDraft <- biTableDraft[, input$travelMean, drop=FALSE]
-      #t <- names(biTableDraft)
-      #biTableDraft <- biTableDraft[!(biTableDraft$dimnames$AreaCode == 'MeanCode' & biTableDraft$dimnames$MeanCode == 'AreaCode'), ]
-      #biTableDraft <- biTableDraft[!(biTableDraft$MeanCode == 'MeanCode' & biTableDraft$AreaCode == 'AreaCode'), ]
-      })
-        
-    
-    output$text1 <- renderText({paste("Travel mean: ", input$travelMean, collapse = ',')})
-    output$text2 <- renderText({paste("Selected ", input$categories, " categories")})
-    
-    output$oneMap <- renderPlot(singleMap(input$categories, input$travelMean, input$classIntMethod))
-    output$biMap  <- renderPlot(singleMap(input$categories, input$travelMean, input$classIntMethod))
-
-#     output$choseMeans <- renderUI({
-#       colnames <- names(biTableDraft$MeanCode)
-#     })
+  })
+  
+  
+  
+  #    renderTable({
+  #       updatebiTable()}
+  #       , include.rownames = FALSE
+  #       , options = list(paging = FALSE, searching = FALSE)
+  #       , caption = paste("Travel mean: ", input$travelMean, 1)
+  #       , caption.placement = getOption("xtable.caption.placement", "top")
+  #       , caption.width = getOption("xtable.caption.width", NULL)
+  #     )
+  
+  output$text1 <- renderText({paste("Travel mean: ", input$travelMean, collapse = ',')})
+  #output$text1 <- renderText({paste("Travel mean: ", input$travelMean)})
+  output$text2 <- renderText({paste("Selected ", input$categories, " categories")})
+  
+  output$oneMap <- renderPlot(singleMap(input$categories, input$travelMean, input$classIntMethod))
+  output$biMap  <- renderPlot(singleMap(input$categories, input$travelMean, input$classIntMethod))
+  
 }
 
-
-
-#Definte UI for the application
-ui <- shinyUI(pageWithSidebar(
-        headerPanel(""),
-        
-        sidebarPanel(
-            uiOutput("categories")
-          , br()
-          , uiOutput("classIntMethod")
-          , br()
-          , uiOutput("travelMean")
-        )  
-        , mainPanel(
-          
-          h3("Map of New Zealand", align = "center")
-          , h4(textOutput("text1"), align = "center")
-          , h4(textOutput("text2"), align = "center")
-          , h4("Data is in percentages", align = "center")
-          
-          , tabsetPanel(#type = "tabs",
-              tabPanel("Single-Mean Table", tableOutput("onetable")  )
-            , tabPanel("Single-Mean Plot", plotOutput("oneMap"))
-            , tabPanel("Two-Mean Table", tableOutput("biTable")  )
-            , tabPanel("Two-Mean Plot", plotOutput("biMap"))
-          )
-          
-          , position="center"
-          , width= "auto"
-          , height= "auto"
-        )       
-    )
-)
-          
-
 shinyApp(ui = ui, server = server)
-
-
-
-# from server part
-# JUST TEMPORARY
-#     observe({
-#       if (length(input$travelMean) > maxGrp)
-#       {
-#         updateCheckboxGroupInput(session, 'travelMean', selected = tail(input$travelMean,maxGrp))
-#       }
-#     })
